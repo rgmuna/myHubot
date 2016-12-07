e54b7582fd22609902d0ade160838911e05ad9cb
rgmuna <rgmuna@gmail.com>
2016-12-06 20:07:41 -0800

    version 2 commit

--- scripts/roquebot.js
+++ scripts/roquebot.js
@@ -2,7 +2,56 @@ var WIKI_API_URL, WIKI_EN_URL, createURL, wikiRequest;
 WIKI_API_URL = "https://en.wikipedia.org/w/api.php";
 WIKI_EN_URL = "https://en.wikipedia.org/wiki";
 
+var numTurns = 0;
+var ranNum = 0;
+var playingGame = 0;
+
 module.exports = function(robot){
+
+//returns what hubot is capable of
+  robot.hear(/what can you do\s?\?/i, function(msg){
+    msg.send("I can do a number of things, including:" + '\n' +
+              "cmd: is class over?     result: how much time is left in class" + '\n' +
+              "cmd: compliment me      result: will probably send a compliment" + '\n' +
+              "cmd: wiki search ____   result: will return a wiki search" + '\n' +
+              "cmd: wiki summary ____  result: will return a summary of a wiki article" + '\n' +
+              "cmd: play game          result: starts a number guessing game"
+            )
+  })
+//hubot guess the number game intro
+  robot.hear(/play game/i, function(msg){
+    numTurns = 0;
+    playingGame = 1;
+    ranNum = Math.ceil(Math.random()*10);
+    msg.send("Welcome to my number guessing game! " + '\n' + '\n' +
+            "See how long it takes you to guess my number between 1 and 10." + '\n' + '\n' +
+            "Respond by saying 'I guess number (number)' and I'll tell you if you're correct." + '\n' + '\n' +
+            "If you ever want to restart, just say 'Play Game'. Let's play!" + '\n' + '\n' +
+            "Start with:  'I guess number ___' ");
+
+  })
+//hubot guess the number gameplay
+  robot.hear(/I guess number (.*)/i, function(msg){
+    numGuess = msg.match[1];
+    var a = parseInt(numGuess);
+
+    if(playingGame === 0){
+      msg.send("You haven't started a game yet!");
+    }
+    else if(a > 10 || a < 1){
+      msg.send("Guess a number between 1 and 10!");
+      return numTurns += 1;
+    }
+    else if(a === ranNum){
+      numTurns += 1;
+      msg.send("You guessed it! It took you " + numTurns + " turns.");
+      return playingGame = 0;
+    }
+    else{
+      msg.send(a + " is WRONG! Guess again!");
+      return numTurns += 1;
+    }
+  })
 
 //Is class over hubot
   robot.hear(/is class over\s?\?/i, function(msg){
@@ -27,7 +76,7 @@ module.exports = function(robot){
 })
 
 //compliment hubot
-robot.hear(/compliment me/i, function(msg){
+robot.respond(/compliment me/i, function(msg){
   var compliments = [
     "You're looking great today!",
     "Hey there good-lookin'!",
@@ -35,8 +84,9 @@ robot.hear(/compliment me/i, function(msg){
     "No.",
     "I wish I could."
   ];
-  var complNum = Math.floor(Math.random()*compliments.length);
-  msg.send(compliments[complNum]);
+  msg.send(msg.random(compliments));
+
+
 })
 
 
@@ -119,7 +169,5 @@ wikiRequest = function(res, params, handler) {
   });
 
 //must include a .respond
-
-
 
 }
